3e7f621892e5cf3c2b5be75edfca6cc0c465a249
Gaster2678 <filipp.unix@gmail.com>
2023-05-12 12:54:10 +0700

    finish tablen player

--- src/module/formation_of_player_data.py
+++ src/module/formation_of_player_data.py
@@ -5,8 +5,8 @@ connect_bd = sqlite3.connect(r'./src/Database/Bunker_play.db')
    
 class data_generation():
 
-    async def sd(user_id):
-        await database_managment_table_player_out_checks.checks_if(user_id=user_id)
+    async def start(user_id, nick):
+        await database_managment_table_player_out_checks.checks_if(user_id=user_id, nick=nick)
 
 class database_managment_table_player_out_checks():
     
@@ -38,13 +38,13 @@ class database_managment_table_player_out_checks():
         connect_bd.commit()
         return data_card
 
-    async def checks_if(user_id):
+    async def checks_if(user_id, nick):
         y = 0
         data = await database_managment_table_player_out_checks.request(data_player=user_id)
         player_card = await database_managment_table_player_out_checks.request_2(data_player=user_id)
         data_return = ["1","2","3","4","5","6","7"]
         await database_managment_table_player_out_checks.chek_proff(data=data, data_return=data_return,data_card=player_card )
-        print(f"{user_id}. {data_return[0]}|{data_return[1]}|{data_return[2]}|{data_return[3]}|{data_return[4]}|{data_return[5]}|{data_return[6]}|")
+        print(f"<@{nick}>. {data_return[0]}|{data_return[1]}|{data_return[2]}|{data_return[3]}|{data_return[4]}|{data_return[5]}|{data_return[6]}|")
 
 
     async def chek_proff(data, data_return, data_card):
@@ -102,21 +102,5 @@ class database_managment_table_player_out_checks():
         else:
             data_return[6] = data_card[0][7]
 
-    async def next_value_player():
-        
-        cur = connect_bd.cursor()
-        cur.execute(f"""UPDATE Player_Cards_check 
-                        SET 
-                        Profession = "1",
-                        Biology = "1",
-                        Health = "1",
-                        Luggage = "1",
-                        Evidense = "1",
-                        Special_conditions = "1",
-                        Hobby = "1"
-                        where user_id = "10" """)
-        connect_bd.commit()
-        cur.close()
-
 #asyncio.run(database_managment_table_player_out_checks.next_value_player())
-asyncio.run(data_generation.sd(10))
+#asyncio.run(data_generation.sd(10))
--- src/module/play.py
+++ src/module/play.py
@@ -1,4 +1,7 @@
 import asyncio
+from formation_of_player_data import data_generation
+
+list_play = ((10,9),(24,99),(25,999))
 
 class raund_1():
     pass
@@ -19,7 +22,15 @@ class Character_Card_table():
     
     async def heading():
         print("Nikname|Profession|Biology|Health|Hobby|Luggage|Evidense|Special_conditions")
+        print("---------------------------------------------------------------------------")
 
     async def table_player(list_of_players):
-        pass
+        await Character_Card_table.heading()
+        for player in list_of_players:
+            user_id = player[0]
+            ping_nick = player[1]
+            
+            await data_generation.start(user_id=user_id,nick= ping_nick)
 
+
+asyncio.run(Character_Card_table.table_player(list_of_players=list_play))
