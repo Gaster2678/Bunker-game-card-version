f76e9fd5f48ecf7b5928ea29b96be0f0686215f1
Gaster2678 <filipp.unix@gmail.com>
2023-05-13 15:56:00 +0700

    1

--- src/module/formation of player data.py
+++ src/module/formation of player data.py
@@ -27,21 +27,40 @@ class database_managment_table_player_out_checks():
 
         return data_checks
     
-    async def checks_if():
-        
-        data = await database_managment_table_player_out_checks.request()
+    async def request_2(data_player):
+        cur = connect_bd.cursor()
+        user_id = data_player[0]
 
+
+        cur.execute(f"""
+            SELECT *
+            FROM Player_Cards
+            where user_id = {user_id}
+        """)
+
+        data_checks = cur.fetchall()
+
+        connect_bd.commit()
+
+        return data_checks
+    
+    async def checks_if(data_player):
+        
+        data = await database_managment_table_player_out_checks.request(data_player=data_player)
+        data_name = await database_managment_table_player_out_checks.request_2(data_player=data_player)
         data_return = ["1","2","3","4","5","6","7"]
 
-        await database_managment_table_player_out_checks.chek_proff(data=data, data_return= data_return)
+        await database_managment_table_player_out_checks.chek_proff(data=data, data_return= data_return, data_retyrn_2=data_name)
         
 
-    async def chek_proff(data, data_return):
-        if data == 0:
+    async def chek_proff(data, data_return, data_retyrn_2):
+        if data[0] == 0:
             database_managment_table_player_out_checks.chek_bio()
         else:
-            data_return[0].replase("1", f"")
-    async def chek_bio(data, data_return):
+            data_return[0].replase("1", f"{data_retyrn_2[0]}")
+            database_managment_table_player_out_checks.chek_bio()
+
+    async def chek_bio(data, data_return, data_retyrn_2):
         pass
 
     async def chek_heal(data, data_return):
